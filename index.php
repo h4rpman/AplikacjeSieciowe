8e1719a9f58acfc3fb0628b1f4de79ae841c904a
h4rpman <karol.czopka@gmail.com>
2021-10-23 17:26:43 +0200

    Comment added

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         <?php
         // put your code here
+        // metan etan propan butan
         ?>
     </body>
 </html>
